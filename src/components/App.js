3b10a401b05becccbac560d9de26419014686186
molfuh <mollyfuhrman@gmail.com>
2022-10-17 21:55:35 -0700

    Tried to fix animation on pageload issue - unsuccessful.

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,8 @@ import '../index.css';
 // import './light.css';
 // import './dark.css';
 import DarkModeToggleFunc from './DarkModeToggleFunc.jsx';
-// import useDarkMode from 'use-dark-mode';
 
 export default function App () {
-  // const darkMode = useDarkMode(false);
   return (
     <>
       <DarkModeToggleFunc />
--- src/components/DarkModeToggleFunc.jsx
+++ src/components/DarkModeToggleFunc.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import useDarkMode from 'use-dark-mode';
-// import DarkModeToggle from "react-dark-mode-toggle";
-// import Toggle from './Toggle.jsx';
 import DarkModeToggle from "react-dark-mode-toggle";
 
 export default function DarkModeToggleFunc () {
-    const darkMode = useDarkMode(false);
-      return (
-        <div>
-          <button type="button" onClick={darkMode.disable}>
-            ☀
-          </button>
-          <DarkModeToggle checked={darkMode.value} onChange={darkMode.toggle} />
-          <button type="button" onClick={darkMode.enable}>
-            ☾
-          </button>
-        </div>
-      );
-  };
-  
-  // export default DarkModeToggleFunc;
+  const darkMode = useDarkMode(false);
+  return (
+    <div>
+      <button type="button" onClick={darkMode.disable}>
+        ☀
+      </button>
+      <DarkModeToggle checked={darkMode.value} onChange={darkMode.toggle} />
+      <button type="button" onClick={darkMode.enable}>
+        ☾
+      </button>
+    </div>
+  );
+};
