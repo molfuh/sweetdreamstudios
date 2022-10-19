cf717b4cbc23795abc4265aa2a013c61c6cf0a33
Molly Fuhrman <mollyfuhrman@gmail.com>
2022-10-19 11:56:50 -0700

    Added clouds. Adjusted positioning of screen/background.

--- src/components/SunMoon.jsx
+++ src/components/SunMoon.jsx
@@ -12,24 +12,83 @@ export default function SunMoon ({darkMode}) {
 
   return (
     <>
-        {darkMode.value ? <Stars><Moon/></Stars> : <><Sun /><Cloud/></>}
+        {darkMode.value ? <><Stars/><Moon/></> : <><Cloud /><Cloud2/><Cloud3/><Sun /></>}
     </>
   );
 };
 
-const Cloud = styled.div`
-    background-color: white;
-    height: 30px;
-    width: 30px;
+
+// const Cloud = styled.div`
+//     background-color: white;
+//     height: 30px;
+//     width: 30px;
+// `;
+
+
+const CloudsScroll = keyframes`
+  0% {transform:  translate(-350px,0);}
+  100% {transform:  translate(100vw,0);}
 `;
 
 const Stars = styled.div`
     background-image: url(${stars});
     height: 30vh;
-    opacity: 0.5;
     width: 100vw;
+    position: relative;
 `;
 
+const Cloud = styled.div`
+    /* background-image: url(${stars}); */
+    /* height: 30vh;
+    width: 20vw;
+    position: relative;
+    background-color: white;
+    position: absolute; */
+    width: 350px;
+    height: 120px;
+    background: white;
+    border-radius: 100px;
+    position: absolute;
+    margin: 320px auto 20px;
+    animation-name: ${CloudsScroll};
+animation-duration: 60s;
+animation-delay: 2s;
+animation-fill-mode: backwards;
+animation-iteration-count: infinite;
+animation-timing-function: linear;
+    &:after {
+      width: 100px;
+      height: 100px;
+	    top: -50px;
+      left: 50px;
+      border-radius: 100px;
+    }
+    &:before {
+      width: 180px;
+      height: 180px;
+	    top: -90px;
+      right: 50px;
+	    border-radius: 200px;
+    }
+    &:after, &:before {
+      content: '';
+      position: absolute;
+      background: white;
+      z-index: -1
+    }
+`;
+
+const Cloud2 = styled(Cloud)`
+  /* animation-delay: 3s; */
+  top: 100px;
+  animation-direction: reverse;
+  animation-delay: 10s;
+`
+
+const Cloud3 = styled(Cloud)`
+    top: 75px;
+    animation-delay: 20s;
+`
 
 
 
@@ -49,7 +108,7 @@ width: 100px;
 height: 100px;
 // background-color: transparent;
 border-radius: 50%;
-box-shadow: 25px 10px 0px 0px #fff; 
+box-shadow: 25px 10px 0px 0px #fff;
 animation-name: ${PlanetEnter};
 animation-duration: 2s;
 animation-fill-mode: forwards;
